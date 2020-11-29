5d4530c5abbe8bb83c31a4f761372100d42dfaa9
purencool <purencool@gmail.com>
2020-11-30 05:53:19 +1100

    Adding default config

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const globalConfiguration = {
+  "default":"default config is working",
+  "default_rest_api": "https://api.github.com",
+  "default_rest_url": "https://api.github.com",
+  "default_rest_issues": "https://api.github.com/purencool/purencool_studio/issues",
+  "default_rest_support_articles": "https://www.purencool.digital/support/api"
+  };
+export default  globalConfiguration;
